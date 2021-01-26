925bfb403f0dd98a132a31c0b5fbda462d1573a4
jandadav <janda.david@gmail.com>
2021-01-26 12:46:55 +0100

    text output
    Signed-off-by: jandadav <janda.david@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,55 +1,93 @@
 let jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 let axios = require('axios');
+const fs = require('fs');
+
+let results = [];
+
+getChangelog(results);
 
 
 
-
-axios.get('https://github.com/zowe/api-layer/compare/v1.19.1...v1.19.2')
+async function getChangelog(results) {
+    await axios.get('https://github.com/zowe/api-layer/compare/v1.18.1...v1.19.2')
     .then((response) => {
         if(response.status === 200) {
         const html = response.data;
         const { window } = new JSDOM(html);
-        var $ = require('jquery')(window);
+        let $ = require('jquery')(window);
         
         $(".pr-1").each( (index, element) => {
 
             
             //console.log($(element).find("code").text());
-            console.log($(element).find("code").find("a").text());
+            let name = $(element).find("code").find("a").text();
+            let title = $(element).find("code").find("a").attr("title");
+            
+            
             
             let commitHref = $(element).find("code").find("a").attr('href');
             commitHref = commitHref != null ? "https://github.com" + commitHref : null
             let prHref = $(element).find(".issue-link").attr('href');
-            
-            console.log(commitHref);
-            console.log(prHref);
-            
 
-            if(prHref != null) {
-                axios.get(prHref).then((response) => {
-                    if(response.status === 200) { 
-                        console.log(prHref + " retrieved")}
-                        const html = response.data;
-                        const { window } = new JSDOM(html);
-                        var $ = require('jquery')(window);
+            // if(prHref != null) {
+            //     getPr(prHref)
+            // } else if (commitHref != null) {
+            //     getCommit(commitHref);
+            // }
 
-                        console.log($(".partial-discussion-header").find(".head-ref").text());
+            if (name) {
+                console.log(name);
+                console.log(title);
+                console.log(commitHref);
+                console.log(prHref);
 
-                }, (error) => console.log(error) );
-            } else if (commitHref != null) {
-                axios.get(commitHref).then((response) => {
-                    if(response.status === 200) { 
-                        console.log(commitHref + " retrieved")}
-                }, (error) => console.log(error) );
+                results.push({"name": name, "title": title,"commit": commitHref, "pr": prHref})
+                console.log("==============================");
             }
-
-
-
-            console.log("+++++++++++++=");
+   
+            //console.log(results);
+            
 
         })
         
+        //console.log(results);
+
+        fs.writeFile("output.json", JSON.stringify(results), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+
+
         
     }
     }, (error) => console.log(err) );
+}
+
+
+
+
+function getPr(prHref) {
+    axios.get(prHref).then((response) => {
+        if(response.status === 200) { 
+            console.log(prHref + " retrieved")}
+            const html = response.data;
+            const { window } = new JSDOM(html);
+            var $ = require('jquery')(window);
+
+            console.log($(".partial-discussion-header").find(".commit-ref").text());
+            
+            return "RETURNED";
+
+    }, (error) => console.log(error) );
+}
+
+function getCommit(commitHref) {
+    axios.get(commitHref).then((response) => {
+        if(response.status === 200) { 
+            console.log(commitHref + " retrieved")}
+    }, (error) => console.log(error) );
+    console.log("+++++++++++++=");
+}
